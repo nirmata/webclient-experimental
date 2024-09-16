011da5e633d20032ff4f5919d969b7ae61736e99
piyushgupte007 <piyush.gupte@nirmata.com>
2024-09-16 18:47:03 +0530

    adding proxy to assets folder to /npm path

--- NPM-NDP-POC/vite.config.ts
+++ NPM-NDP-POC/vite.config.ts
@@ -4,5 +4,13 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {},
+  server: {
+    proxy: {
+      '/assets': {
+        target: 'https://devtest1.nirmata.co/npm',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/assets/, '/assets'),
+      }
+    }
+  },
 });
